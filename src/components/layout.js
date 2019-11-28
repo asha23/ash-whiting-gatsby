3c64b40c045977cb43b19b468dde80bb234de806
Ash Whiting <ash@digitaltactics.co.uk>
2019-11-28 16:32:56 +0000

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { useStaticQuery, Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+
+export default ({ children }) => {
+	const data = useStaticQuery(
+		graphql`
+			query {
+				site {
+				siteMetadata {
+					title
+				}
+				}
+			}
+		`
+	)
+
+	return (
+		<div
+			css={css`
+				
+				padding:10px;
+        		max-width: 1200px;
+        		padding: ${rhythm(2)};
+				padding-top: ${rhythm(1.5)};
+				background:#fafafa;
+      		`}
+		>
+			<div 
+				className="header-logo"
+				css={css`
+					display:inline-block;
+					vertical-align:middle;
+					width:50%;
+				`}
+			>
+				<Link to={`/`}>
+					<h1
+						css={css`
+              				margin-bottom: ${rhythm(2)};
+            			`}
+					>
+						{data.site.siteMetadata.title}
+					</h1>
+				</Link>
+			</div>
+
+			<div 
+				className="menu"
+				css={css`
+					display:inline-block;
+					vertical-align:middle;
+					width:50%;
+					text-align:right;
+				`}
+			>
+				<Link
+					to={`/about/`}
+					css={css`
+						padding: 0 10px;
+						font-family: 'Montserrat',sans-serif;
+						font-weight:normal;
+          			`}
+				>
+					About
+        		</Link>
+
+				{/* <Link
+					to={`/my-files/`}
+					css={css`
+						padding: 0 10px;
+						font-family: 'Montserrat',sans-serif;
+						font-weight:normal;
+          			`}
+				>
+					Site Files
+        		</Link> */}
+			</div>
+
+			<div className="main-content">
+				{children}
+			</div>
+			
+
+		</div>
+	)
+}
---
+++ src/pages/index.js
@@ -0,0 +1,85 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+	<Layout>
+		<div>
+			<h2 css={css`
+					display:inline-block;
+		  		`}
+			>
+				A web development blog
+		  	</h2>
+
+			<h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+
+			{data.allMarkdownRemark.edges.map(({ node }) => (
+				<div key={node.id}>
+					<Link
+						to={node.fields.slug}
+					>
+						<h3
+							css={css`
+				 				margin-bottom: ${rhythm(1 / 4)};
+			   				`}
+						>
+							{node.frontmatter.title}{" "}
+							<span
+								css={css`
+				   					color: #bbb;
+				 				`}
+							>
+								— {node.frontmatter.date}
+							</span>
+						</h3>
+					</Link>
+					<p>Time to read: {node.timeToRead} min</p>
+					<p>{node.excerpt}</p>
+					<div
+						css={css`
+							width:100%;
+							text-align:right;
+							margin-bottom:20px;
+							padding-bottom:20px;
+							border-bottom:1px solid #efefef;
+							font-weight:bold;
+						`}
+					>
+						<Link
+							to={node.fields.slug}
+
+						>
+							Read More
+						</Link>
+					</div>
+				</div>
+			))}
+
+		</div>
+	</Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+		  timeToRead
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+		  fields {
+			  slug
+		  }
+          excerpt
+        }
+      }
+    }
+  }
+`
